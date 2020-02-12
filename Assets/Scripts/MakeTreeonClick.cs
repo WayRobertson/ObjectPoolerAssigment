2c84f66900cad8bfb4967abb7d59cd4f860a29d5
Way Robertson <mizepie@hotmail.com>
2020-02-12 17:09:40 -0500

    Way: Input script with Object List
    This script handles mouse input and the object pooler which I am still working on. But it has the list component that is visible in the inspector.

---
+++ Assets/Scripts/MakeTreeonClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeTreeonClick : MonoBehaviour
+{
+    public GameObject treePrefab;
+    public List<GameObject> trees = new List<GameObject>();
+   
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Vector3 whereWeClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            whereWeClick = new Vector3(whereWeClick.x, whereWeClick.y, 0);
+            treePrefab = Instantiate(treePrefab, whereWeClick, Quaternion.identity);
+            trees.Add(treePrefab);
+        }
+    }
+}
