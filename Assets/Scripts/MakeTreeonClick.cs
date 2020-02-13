a2eb84feadda0d51bdcd77257cc2d34f0cc1eaa9
Way Robertson <mizepie@hotmail.com>
2020-02-13 16:30:27 -0500

    Way Update: SampleScene, MakeTreeonClick script, TreeGenerator script
    I am in the process of adding the Object Pooler to the MakeTreeonClick script.

--- Assets/Scripts/MakeTreeonClick.cs
+++ Assets/Scripts/MakeTreeonClick.cs
@@ -6,16 +6,42 @@ public class MakeTreeonClick : MonoBehaviour
 {
     public GameObject treePrefab;
     public GameObject listcomponent;
-    public List<GameObject> trees = new List<GameObject>();
-     
+    public int initialTreepool;
+    public List<GameObject> treePool = new List<GameObject>();
+
+    void Start()
+    {
+        treePool = new List<GameObject>();
+        for (int i = 0; i < initialTreepool; i++)
+        {
+            if (!treePool[i].activeInHierarchy)
+            {
+                GameObject feebee = GameObject.Instantiate(listcomponent);
+                feebee.SetActive(false);
+                treePool.Add(feebee);
+                //feebee is a GameObject temp name. Remember to change!
+            }
+        }
+    }
+#region MouseInput
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             Vector3 whereWeClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             whereWeClick = new Vector3(whereWeClick.x, whereWeClick.y, 0);
             listcomponent = Instantiate(treePrefab, whereWeClick, Quaternion.identity);
-            trees.Add(listcomponent);
         }
     }
+  #endregion  
+    // public GameObject AddTree()
+    // {
+    //     for (int i = 0; i < treePool.Count; i++)
+    //     {
+    //         if (!treePool[i].activeInHierarchy)
+    //         {
+    //             return treePool[i];
+    //         }
+    //     }
+    // }
 }
--- Assets/Scripts/TreeGenerator.cs
+++ Assets/Scripts/TreeGenerator.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script is for generating trees with the line renderer.
+// "yeild retun new WaitForSeconds"(?). ANS: After some seconds pass inbetween the last line of code and this one... create this branch
 public class TreeGenerator : MonoBehaviour
 {
     public GameObject treelimb;
@@ -14,6 +16,7 @@ public class TreeGenerator : MonoBehaviour
     IEnumerator CreateTree()
     {
         // yield return new WaitForSeconds(0.2f);
+        //creates three initial branches if the gameobject does not have a parent. The first three can be consider the trunk.
         if (transform.parent == null)
         {
             yield return new WaitForSeconds(0.1f);
@@ -26,6 +29,7 @@ public class TreeGenerator : MonoBehaviour
 
             MakeBranch();
         }
+        // creates a new branch from the previous branches if its parent does not have a parent.
         else if (transform.parent.parent == null)
         {
             yield return new WaitForSeconds(0.1f);
@@ -36,11 +40,13 @@ public class TreeGenerator : MonoBehaviour
             MakeBranch();
         }
         else if (transform.parent.parent.parent == null)
+        //and the cycle continues.
         {
             yield return new WaitForSeconds(0.1f);
 
             MakeBranch();
         }
+        //if this branch does not have a great grand parent then create another branch, otherwise no more branches.
         else if (transform.parent.parent.parent.parent == null)
         {
             yield return new WaitForSeconds(0.1f);
